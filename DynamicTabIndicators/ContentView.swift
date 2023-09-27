a2a819479b45ade87fe5826077a557f138591618
Yunus Emre Taşdemir <yetsdmr@gmail.com>
2023-09-27 18:09:56 +0300

    Initial Commit

---
+++ DynamicTabIndicators/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  DynamicTabIndicators
+//
+//  Created by Yunus Emre Taşdemir on 27.09.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ DynamicTabIndicators/DynamicTabIndicatorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  DynamicTabIndicatorsApp.swift
+//  DynamicTabIndicators
+//
+//  Created by Yunus Emre Taşdemir on 27.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct DynamicTabIndicatorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
